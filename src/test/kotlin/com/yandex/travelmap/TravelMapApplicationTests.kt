cf80b7c2438d51d2bf14da703d340d90e10f4cab
Nikita Bosov <neckbosov@gmail.com>
2021-03-10 13:02:25 +0300

    Create template

---
+++ src/test/kotlin/com/yandex/travelmap/TravelMapApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yandex.travelmap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TravelMapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
