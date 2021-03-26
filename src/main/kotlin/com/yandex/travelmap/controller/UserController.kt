6984c22ba7e52119d1e35322d1b2debd60cf6068
Nikita Bosov <neckbosov@gmail.com>
2021-03-26 17:29:15 +0300

    remove username parameter

--- src/main/kotlin/com/yandex/travelmap/controller/UserController.kt
+++ src/main/kotlin/com/yandex/travelmap/controller/UserController.kt
@@ -2,24 +2,33 @@ package com.yandex.travelmap.controller
 
 import com.yandex.travelmap.dto.CountryResponse
 import com.yandex.travelmap.dto.VisitedCountryRequest
+import com.yandex.travelmap.exception.NotAuthorizedException
 import com.yandex.travelmap.service.UserService
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/user")
 class UserController(private val userService: UserService) {
-    @GetMapping("/{username}/visited_countries")
-    fun getVisitedCountries(@PathVariable username: String): List<CountryResponse> {
-        return userService.getVisitedCountries(username)
+
+    private fun getCurrentUser(): UserDetails {
+        return SecurityContextHolder.getContext().authentication?.principal as? UserDetails
+            ?: throw NotAuthorizedException()
     }
 
-    @PutMapping("/{username}/visited_countries")
-    fun addVisitedCountry(@RequestBody request: VisitedCountryRequest, @PathVariable username: String) {
-        return userService.addVisitedCountry(username, request)
+    @GetMapping("/visited_countries")
+    fun getVisitedCountries(): List<CountryResponse> {
+        return userService.getVisitedCountries(getCurrentUser().username)
     }
 
-    @DeleteMapping("/{username}/visited_countries")
-    fun deleteVisitedCountry(@RequestBody request: VisitedCountryRequest, @PathVariable username: String) {
-        return userService.deleteVisitedCountry(username, request)
+    @PutMapping("/visited_countries")
+    fun addVisitedCountry(@RequestBody request: VisitedCountryRequest) {
+        return userService.addVisitedCountry(getCurrentUser().username, request)
+    }
+
+    @DeleteMapping("/visited_countries")
+    fun deleteVisitedCountry(@RequestBody request: VisitedCountryRequest) {
+        return userService.deleteVisitedCountry(getCurrentUser().username, request)
     }
 }
